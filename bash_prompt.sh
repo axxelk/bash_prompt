0ad2b1ae4d98e6e01758f212feba0f1de4092c1c
Axel <axel@axelk.de>
2022-11-30 16:57:24 +0100

    some changes

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -1,5 +1,5 @@
 # root@f36:/etc/profile.d $ cat bash_prompt.sh
-promptuserlist="yourloginnamehere othernamehere"
+promptuserlist="root axelk"
 xlogname=$(logname)
 userisin=$(echo $promptuserlist|grep -w -q $xlogname && echo "true" || echo "false")
 
@@ -7,7 +7,7 @@ promptfunction ()
 {
     if [ "$USER" = "root" ]; then
         export PS1="\e[1;31m\]\u\e[1;30m\]@\h:\w \\$ \[\e[0m\]";
-    elif [ "$USER" = "lnx" ]; then
+    elif [ "$USER" = "appuser" ]; then
         export PS1="\e[1;33m\]\u\e[1;30m\]@\h:\w \\$ \[\e[0m\]";
     else
         export PS1="\e[1;32m\]\u\e[1;30m\]@\h:\w \\$ \[\e[0m\]";
